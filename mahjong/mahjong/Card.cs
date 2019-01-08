6cf17ab1b953ab1a8555dd19a7a090810452c526
yuyang huang <410644548@qq.com>
2019-01-08 22:23:35 +0800

    start

---
+++ mahjong/mahjong/Card.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mahjong
+{
+    /// <summary>
+    /// 描述一个花色的牌
+    /// </summary>
+    class Color
+    {
+        /// <summary>
+        /// 允许的雀头，长沙麻将中为2, 5, 8
+        /// </summary>
+        public static int[] ChangshaValidPairs = new int[3] { 2, 5, 8 };
+
+        /// <summary>
+        /// 允许所有雀头
+        /// </summary>
+        public static int[] NormalValidPairs = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        /// <summary>
+        /// 当前程序中使用的规则
+        /// </summary>
+        public static int[] ValidPairs = ChangshaValidPairs;
+
+        /// <summary>
+        /// 牌的有序字典
+        /// </summary>
+        public SortedDictionary<int, int> cards = new SortedDictionary<int, int>();
+
+        /// <summary>
+        /// 总张数
+        /// </summary>
+        public int cnt = 0;
+
+        /// <summary>
+        /// 插入一张牌
+        /// </summary>
+        /// <param name="point"> 牌的点数 </param>
+        /// <returns> 是否成功删除 </returns>
+        public bool Insert(int point)
+        {
+            if (!cards.ContainsKey(point))
+            {
+                cards[point] = 0;
+            }
+
+            if (cards[point] < 4)
+            {
+                cards[point] = cards[point] + 1;
+                cnt++;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 删除一张牌
+        /// </summary>
+        /// <param name="point"> 牌的点数 </param>
+        /// <returns> 是否成功删除 </returns>
+        public bool Delete(int point)
+        {
+            if (!cards.ContainsKey(point))
+            {
+                return false;
+            }
+
+            if (cards[point] > 0)
+            {
+                cards[point]--;
+                if (cards[point] == 0)
+                {
+                    cards.Remove(point);
+                }
+
+                cnt--;
+                return true;
+            } else
+            {
+                cards.Remove(point);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 清空所有牌
+        /// </summary>
+        public void Clear()
+        {
+            cards.Clear();
+            cnt = 0;
+        }
+
+        /// <summary>
+        /// 判断是否可以拆成1雀头+若干面子
+        /// </summary>
+        /// <returns></returns>
+        public bool HuWithPair()
+        {
+            if (cnt % 3 != 2)
+            {
+                return false;
+            }
+
+            foreach (int pair in ValidPairs)
+            {
+                if (cards.ContainsKey(pair) && cards[pair] >= 2)
+                {
+                    Delete(pair);
+                    Delete(pair);
+
+                    bool hu = HuWithoutPair();
+
+                    Insert(pair);
+                    Insert(pair);
+
+                    if (hu)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 判断是否可以拆成若干面子
+        /// </summary>
+        /// <returns></returns>
+        public bool HuWithoutPair()
+        {
+            if (cnt % 3 != 0)
+            {
+                return false;
+            }
+
+            SortedDictionary<int, int> temp = new SortedDictionary<int, int>();
+            foreach (var key in cards.Keys)
+            {
+                temp[key] = cards[key];
+            }
+            for (int i = 0; i < cnt / 3; i++)
+            {
+                int point = temp.First().Key;
+                int cnt = temp.First().Value;
+                if (cnt >= 3)
+                {
+                    temp[point] -= 3;
+                    if (temp[point] == 0)
+                    {
+                        temp.Remove(point);
+                    }
+                } else
+                {
+                    if (point < 8 && temp.ContainsKey(point + 1) && temp.ContainsKey(point + 2) && temp[point + 1] > 0 && temp[point + 2] > 0)
+                    {
+                        for (int j = point; j <= point + 2; j++) {
+                            temp[j]--;
+                            if (temp[j] == 0)
+                            {
+                                temp.Remove(j);
+                            }
+                        }
+                    } else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 听了哪些牌
+    /// </summary>
+    class Ting
+    {
+        /// <summary>
+        /// 条，筒，万分别听了哪些牌
+        /// </summary>
+        public List<int>[] points = new List<int>[3] { new List<int>(), new List<int>(), new List<int>() };
+
+        /// <summary>
+        /// 重置
+        /// </summary>
+        public void Clear()
+        {
+            foreach (var point in points)
+            {
+                point.Clear();
+            }
+        }
+
+        /// <summary>
+        /// 计算听了几张牌
+        /// </summary>
+        /// <returns> 听了几张牌 </returns>
+        public int Size()
+        {
+            return points[0].Count + points[1].Count + points[2].Count;
+        }
+    }
+
+    /// <summary>
+    /// 描述一个牌区的牌
+    /// </summary>
+    class CardCollection
+    {
+        /// <summary>
+        /// 条，筒，万分别有哪些牌
+        /// </summary>
+        public Color[] colors = new Color[3] { new Color(), new Color(), new Color() };
+
+        /// <summary>
+        /// 清空所有牌
+        /// </summary>
+        public void Clear()
+        {
+            foreach (var color in colors)
+            {
+                color.Clear();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 描述自己的牌
+    /// </summary>
+    class MyCard
+    {
+        /// <summary>
+        /// 手牌
+        /// </summary>
+        public CardCollection hand = new CardCollection();
+
+        /// <summary>
+        /// 副露，吃、碰、杠的牌
+        /// </summary>
+        public CardCollection fulu = new CardCollection();
+
+        /// <summary>
+        /// 清空所有牌
+        /// </summary>
+        public void Clear()
+        {
+            hand.Clear();
+            fulu.Clear();
+        }
+
+        /// <summary>
+        /// 计算是否胡牌
+        /// </summary>
+        /// <returns> 是否胡牌 </returns>
+        public bool CheckHu()
+        {
+            return
+                (hand.colors[0].HuWithPair() && hand.colors[1].HuWithoutPair() && hand.colors[2].HuWithoutPair()) ||
+                (hand.colors[0].HuWithoutPair() && hand.colors[1].HuWithPair() && hand.colors[2].HuWithoutPair()) ||
+                (hand.colors[0].HuWithoutPair() && hand.colors[1].HuWithoutPair() && hand.colors[2].HuWithPair());
+        }
+
+        /// <summary>
+        /// 计算听牌情况
+        /// </summary>
+        /// <param name="ting"> 听了哪些牌 </param>
+        public void CalcTing(Ting ting)
+        {
+            ting.Clear();
+
+            for (int c = 0; c < 3; c++)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    if (hand.colors[c].Insert(i))
+                    {
+                        if (CheckHu())
+                        {
+                            ting.points[c].Add(i);
+                        }
+
+                        hand.colors[c].Delete(i);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算是否听牌
+        /// </summary>
+        /// <returns> 是否听牌 </returns>
+        public bool IsTing()
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                int[] cnt = new int[3]{ 0, 0, 0 };
+                for (int fc = 0; fc < 3; fc++)
+                {
+                    cnt[(hand.colors[fc].cnt + (fc == c ? 1 : 0)) % 3]++;
+                }
+
+                if (cnt[0] != 2 || cnt[2] != 1)
+                {
+                    continue;
+                }
+
+                for (int i = 1; i <= 9; i++)
+                {
+                    if (hand.colors[c].Insert(i))
+                    {
+                        if (CheckHu())
+                        {
+                            hand.colors[c].Delete(i);
+                            return true;
+                        }
+
+                        hand.colors[c].Delete(i);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 计算舍牌之后的听牌情况
+        /// </summary>
+        /// <param name="forgive"> 舍了(花色，数字)之后，听了哪些牌</param>
+        public void CalcForgive(Dictionary<KeyValuePair<int, int>, Ting> forgive)
+        {
+            forgive.Clear();
+            for (int c = 0; c < 3; c++)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    if (hand.colors[c].Delete(i))
+                    {
+                        Ting ting = new Ting();
+                        CalcTing(ting);
+                        if (ting.Size() > 0)
+                        {
+                            forgive[new KeyValuePair<int, int>(c, i)] = ting;
+                        }
+
+                        hand.colors[c].Insert(i);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算当前牌型是几向听的
+        /// </summary>
+        /// <param name="cur"> 当前深度 </param>
+        /// <param name="limit"> 递归最大允许深度 </param>
+        /// <param name="ans"> 当前最优听数 </param>
+        public void GetToTing(int cur, int limit, ref int ans)
+        {
+            if (cur >= limit || cur >= ans)
+            {
+                return;
+            }
+
+            if (IsTing())
+            {
+                ans = cur;
+                return;
+            }
+
+            bool firstJiang = true;
+            for (int c = 0; c < 3; c++)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    bool dothis = hand.colors[c].cards.ContainsKey(i - 1) || hand.colors[c].cards.ContainsKey(i) || hand.colors[c].cards.ContainsKey(i + 1);
+                    if (!dothis)
+                    {
+                        if (firstJiang && Color.ValidPairs.Contains(i))
+                        {
+                            dothis = true;
+                            firstJiang = false;
+                        }
+                    }
+                    if (dothis)
+                    {
+                        if (hand.colors[c].Insert(i))
+                        {
+                            for (int fc = 0; fc < 3; fc++)
+                            {
+                                var keys = hand.colors[fc].cards.Keys.ToList();
+                                foreach (int fi in keys)
+                                {
+                                    if (fc != c || i != fi)
+                                    {
+                                        hand.colors[fc].Delete(fi);
+                                        GetToTing(cur + 1, limit, ref ans);
+                                        hand.colors[fc].Insert(fi);
+                                    }
+                                }
+                            }
+
+                            hand.colors[c].Delete(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ mahjong/mahjong/MainWindow.xaml.cs
@@ -0,0 +1,220 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mahjong
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyCard myCard = new MyCard();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetCard(MyCard myCard)
+        {
+            myCard.Clear();
+
+            foreach (var c in tiao.Text)
+            {
+                if (c >= '1' && c <= '9')
+                {
+                    myCard.hand.colors[0].Insert((int)c - 48);
+                }
+            }
+
+            foreach (var c in tong.Text)
+            {
+                if (c >= '1' && c <= '9')
+                {
+                    myCard.hand.colors[1].Insert((int)c - 48);
+                }
+            }
+
+            foreach (var c in wan.Text)
+            {
+                if (c >= '1' && c <= '9')
+                {
+                    myCard.hand.colors[2].Insert((int)c - 48);
+                }
+            }
+        }
+
+        private void CalcHu(object sender, RoutedEventArgs e)
+        {
+            GetCard(myCard);
+            if (myCard.CheckHu())
+            {
+                result.Text = "胡了";
+            }
+            else
+            {
+                result.Text = "没胡";
+            }
+        }
+
+        private void CalcForgive(object sender, RoutedEventArgs e)
+        {
+            GetCard(myCard);
+
+            Dictionary<KeyValuePair<int, int>, Ting> forgive = new Dictionary<KeyValuePair<int, int>, Ting>();
+            myCard.CalcForgive(forgive);
+            string resultString = "";
+            foreach (var key in forgive.Keys)
+            {
+                resultString += String.Format("舍 {0}{1} : 听 ", key.Value, Util.names[key.Key]);
+                for (int c = 0; c < 3; c++)
+                {
+                    foreach (int p in forgive[key].points[c])
+                    {
+                        resultString += String.Format("{0}{1} ", p, Util.names[c]);
+                    }
+                }
+
+                resultString += "\n";
+            }
+
+            result.Text = resultString;
+        }
+
+        private void CalcTing(object sender, RoutedEventArgs e)
+        {
+            GetCard(myCard);
+
+            int ans = 999;
+            int limit = 3;
+            myCard.GetToTing(0, limit, ref ans);
+            if (ans > 100)
+            {
+                result.Text = String.Format("大于 {0} 向听", limit);
+            }
+            else
+            {
+                result.Text = String.Format("{0} 向听", ans);
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            tiao.Text = tong.Text = wan.Text = "";
+        }
+
+        private void qiwan_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (qiwan.Text.Count() > 0)
+            {
+                char c = qiwan.Text[0];
+                for (int i = 0; i < wan.Text.Count(); i++)
+                {
+                    if (wan.Text[i] == c)
+                    {
+                        wan.Text = wan.Text.Remove(i, 1);
+                        qiwan.Text = "";
+                        return;
+                    }
+                }
+            }
+
+            qiwan.Text = "";
+        }
+
+        private void qitiao_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (qitiao.Text.Count() > 0)
+            {
+                char c = qitiao.Text[0];
+                for (int i = 0; i < tiao.Text.Count(); i++)
+                {
+                    if (tiao.Text[i] == c)
+                    {
+                        tiao.Text = tiao.Text.Remove(i, 1);
+                        qitiao.Text = "";
+                        return;
+                    }
+                }
+            }
+
+            qitiao.Text = "";
+        }
+
+        private void qitong_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (qitong.Text.Count() > 0)
+            {
+                char c = qitong.Text[0];
+                for (int i = 0; i < tong.Text.Count(); i++)
+                {
+                    if (tong.Text[i] == c)
+                    {
+                        tong.Text = tong.Text.Remove(i, 1);
+                        qitong.Text = "";
+                        return;
+                    }
+                }
+            }
+
+            qitong.Text = "";
+        }
+
+        private void GetImage(object sender, RoutedEventArgs e)
+        {
+            Util.GetScreen();
+        }
+
+        private void Ana(object sender, RoutedEventArgs e)
+        {
+            Image<Bgr, byte> image = new Image<Bgr, byte>(Util.CurrentPath + @"\screen.png");
+
+            int channels = image.NumberOfChannels;
+            int height = image.Height;
+            int width = image.Width;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+
+                }
+            }
+            screen.Source = ToBitmapSource(image);
+        }
+
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                return bs;
+            }
+        }
+    }
+}
